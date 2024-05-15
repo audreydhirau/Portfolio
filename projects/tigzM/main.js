2089e7f57357c030301be8a4d545a7cd38725c94
audreydhirau <audreydhirau@gmail.com>
2024-05-15 16:19:35 +0200

    done

--- projects/tigzM/main.js
+++
@@ -1,8 +0,0 @@
-const darkTheme = document.getElementById("light")
-const body = document.body
-
-darkTheme.addEventListener("click", function(event){
-    event.preventDefault();
-    body.classList.toggle('black')
-   
-})
